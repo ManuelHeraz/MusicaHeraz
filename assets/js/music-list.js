86bb8f1af05ecb65428ee25b909ef5c17d474e50
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-03-19 20:52:54 -0600

    v24.03.19.0852

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -36,12 +36,118 @@ let allMusic = [
         src: "Fuego-Ligas" //name here
         },
 
+        //añadidos 2024 03 19
+
     {
-        name: "Evil",//music name
-        artist: "Interpol", // artist name
-        img:"Interpol-Evil", // image name
-        src: "Interpol-Evil" //name here
+        name: "Gym Class Heroes",//music name
+        artist: "Patrick Stump", // artist name
+        img:"Gym-Patrick", // image name
+        src: "Gym-Patrick" //name here
         },
+
+    {
+        name: "Viento",//music name
+        artist: "Caifanes", // artist name
+        img:"Viento", // image name
+        src: "Viento" //name here
+    },
+
+    {
+        name: "Buenos Días,Amor",//music name
+        artist: "José José", // artist name
+        img:"Buenos-Jose", // image name
+        src: "Buenos-Jose" //name here
+    },
+
+    {
+        name: "Eres",//music name
+        artist: "Cafe Tacuba", // artist name
+        img:"Eres-Cafe", // image name
+        src: "Eres-Cafe" //name here
+    },
+
+    {
+        name: "Centuries",//music name
+        artist: "Fall Out Boy", // artist name
+        img:"Centuries-FOB", // image name
+        src: "Centuries-FOB" //name here
+    },
+
+    {
+        name: "Sola Nunca Estaras",//music name
+        artist: "Dragon Ball GT", // artist name
+        img:"Sola-GT", // image name
+        src: "Sola-GT" //name here
+    },
+
+    {
+        name: "Glass Walls",//music name
+        artist: "Nikolas Joseph Ammar", // artist name
+        img:"Glass-Niko", // image name
+        src: "Glass-Niko" //name here
+    },
+
+    {
+        name: "Persiana Americana",//music name
+        artist: "La neta no se XD", // artist name
+        img:"Persiana-XD", // image name
+        src: "Persiana-XD" //name here
+    },
+
+    {
+        name: "Química",//music name
+        artist: "Reyno", // artist name
+        img:"Quimica-Reyno", // image name
+        src: "Quimica-Reyno" //name here
+    },
+
+    {
+        name: "Como Tú",//music name
+        artist: "Esteban Galaxias (XD)", // artist name
+        img:"ComoTu-SU", // image name
+        src: "ComoTu-SU" //name here
+    },
+
+    {
+        name: "Whispers In The Dark",//music name
+        artist: "Skillet", // artist name
+        img:"Whispers-Skillet", // image name
+        src: "Whispers-Skillet" //name here
+    },
+
+    {
+        name: "Labios Rotos",//music name
+        artist: "Zoé", // artist name
+        img:"Labios-Zoe", // image name
+        src: "Labios-Zoe" //name here
+    },
+
+    {
+        name: "Get Jinxed",//music name
+        artist: "Onsa Media", // artist name
+        img:"Get-Jinxed", // image name
+        src: "Get-Jinxed" //name here
+    },
+
+    {
+        name: "Sweater Weather",//music name
+        artist: "The Neighbourhood", // artist name
+        img:"SW-N", // image name
+        src: "SW-N" //name here
+    },
+
+    {
+        name: "Major Tom",//music name
+        artist: "¿Me creeras que no recuerdo? XD", // artist name
+        img:"Major-Tom", // image name
+        src: "Major-Tom" //name here
+    },
     
+    {
+        name: "Meltdown",//music name
+        artist: "Ash", // artist name
+        img:"Meltdown-Ash", // image name
+        src: "Meltdown-Ash" //name here
+    },
 
 ];
